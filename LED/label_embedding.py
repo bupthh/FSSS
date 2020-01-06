f05a429def288dfa65afd07e78213a2ccc245101
bupthh <33594254+bupthh@users.noreply.github.com>
2020-01-06 23:48:16 +0800

    Update label_embedding.py
    Add code comments for class of LabelEmbedding

--- LED/label_embedding.py
+++ LED/label_embedding.py
@@ -5,6 +5,12 @@ import numpy as np
 
 
 class LabelEmbedding(object):
+    """
+    This class is for building the file of embedding representation vectors for all label descriptions.
+    we use the Universal Sentence Encoder (https://tfhub.dev/google/collections/universal-sentence-encoder/1) to transform the label text.
+    This encoder uses a transformer-network that is trained on a variety of data sources and a variety of tasks.
+    By inputting a variable length label text, we can get the output of a 512-dimensional vector.
+    """
 
     module_url = "https://tfhub.dev/google/universal-sentence-encoder-large/5" #@param ["https://tfhub.dev/google/universal-sentence-encoder/4", "https://tfhub.dev/google/universal-sentence-encoder-large/5"]
     model = hub.load(module_url)
@@ -12,6 +18,12 @@ class LabelEmbedding(object):
 
     @staticmethod
     def build_embedding_file(label_set, file_url):
+        """
+        build the file of embedding representation vectors for all label descriptions.
+        :param label_set: the list for all labels
+        :param file_url:  the output file to record  embedded vectors for all labels.
+        :return: None
+        """
         message_embeddings = LabelEmbedding.model(label_set)
 
         with open(file_url, 'w') as ef:
@@ -22,4 +34,4 @@ class LabelEmbedding(object):
 
 if __name__ == '__main__':
     bird_labels = ['cock', 'hen', 'ratite, ratite bird, flightless bird', 'ostrich, Struthio camelus']
-    LabelEmbedding.build_embedding_file(bird_labels, 'd:/bird_labels.embed')
+    LabelEmbedding.build_embedding_file(bird_labels, 'bird_labels.embed')
